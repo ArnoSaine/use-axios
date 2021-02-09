8e048df1dbb78be70881c55cc19b8da94f56fb55
Arno Saine <arno@mowhi.com>
2021-02-09 17:11:17 +0200

    Fix: return API as an object from create

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import axios from 'axios';
 import { create as createUseAsync } from '@postinumero/use-async';
 import obsoleteWithReplacement from './utils/obsoleteWithReplacement';
 
-export const create = (config) =>
-  createUseAsync(typeof config === 'function' ? config : axios.create(config));
+export const create = (config) => {
+  const [useAxios, refetch, useAxiosSafe] = createUseAsync(
+    typeof config === 'function' ? config : axios.create(config)
+  );
+  return {
+    useAxios,
+    refetch,
+    useAxiosSafe,
+  };
+};
 
-const [useAxios, refetch, useAxiosSafe] = create(axios);
+const { useAxios, refetch, useAxiosSafe } = create(axios);
 
 export { useAxios as default, useAxios, refetch, useAxiosSafe };
 
